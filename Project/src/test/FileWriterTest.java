c802b4217e5a6ef72486313b06296062cc827015
reyhaneh salkhi <51803127+reyhanehsalkhi@users.noreply.github.com>
2020-02-25 18:12:01 -0500

    Update FileWriterTest.java

--- Project/src/test/FileWriterTest.java
+++ Project/src/test/FileWriterTest.java
@@ -14,30 +14,30 @@ import junit.framework.Assert;
 
 public class FileWriterTest {
 	Hashing hashing = new Hashing();
-	
+
 	@Test
 	public void testNotEquals_Symmetric() throws IOException {
 
-		File f1 = new File("/tmp/temp/sub3");
-		File f2 = new File("/tmp/temp1/sub3");
+		File path1 = new File("/tmp/temp/sub1/sub2");
+		File path2 = new File("/tmp/temp1/sub1/sub2");
 		String text1 = "this is some text";
 		String text2 = "this is some other text";
 		Writer writer = new Writer();
 		try {
-			if (!f1.isDirectory()) {
-				if (f1.mkdirs()) {
-					writer.writeTo("/tmp/temp/sub3/file.txt", text1);
-				}
+			if (!path1.isDirectory()) {
+				if (path1.mkdirs())
+					writer.writeTo(path1 + "/file.txt", text1);
+
 			} else {
-				writer.writeTo("/tmp/temp/sub3/file.txt", text1);
+				writer.writeTo(path1 + "/file.txt", text1);
 			}
 
-			if (!f2.isDirectory()) {
-				if (f2.mkdirs()) {
-					writer.writeTo("/tmp/temp1/sub3/file.txt", text2);
+			if (!path2.isDirectory()) {
+				if (path2.mkdirs()) {
+					writer.writeTo(path2 + "/file.txt", text2);
 				}
 			} else {
-				writer.writeTo("/tmp/temp1/sub3/file.txt", text2);
+				writer.writeTo(path2 + "/file.txt", text2);
 			}
 
 		} catch (Exception e) {
@@ -50,8 +50,8 @@ public class FileWriterTest {
 	@Test
 	public void testEquals_Symmetric() throws IOException {
 
-		File path1 = new File("/tmp/temp/sub3");
-		File path2 = new File("/tmp/temp1/sub3");
+		File path1 = new File("/tmp/temp/sub1/sub2");
+		File path2 = new File("/tmp/temp1/sub1/sub2");
 		String text = "this is some text";
 		Writer writer = new Writer();
 		try {
